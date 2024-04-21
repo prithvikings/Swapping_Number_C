8ccfa91b8b4f0f7de9b7556580fd335791841d7e
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2024-04-21 18:30:25 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -51,3 +51,24 @@ int main(){
     printf("The swap value of a: %d\n",a);
     printf("The swap value of b: %d",b);
 }
+
+//Swapping of two number using int pointer method
+#include<stdio.h>
+int swap(int x,int y){
+    int *p=&x;
+    int *q=&y;
+    *p=*p+*q;
+    *q=*p-*q;
+    *p=*p-*q;
+    printf("The Swap value of a: %d\n",x);
+    printf("The Swap value of b: %d",y);
+}
+int main(){
+    int a;
+    printf("Enter number a: ");
+    scanf("%d",&a);
+    int b;
+    printf("Enter number b: ");
+    scanf("%d",&b);
+    swap(a,b);
+}
