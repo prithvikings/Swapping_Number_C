a163875ccfd2fe1f2e1715af7a492c4618b40a3e
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2024-04-21 18:26:36 +0530

    Added pointer method 1 swap.c

--- swap.c
+++ swap.c
@@ -30,3 +30,24 @@ int main(){
     printf("The swap value of a: %d\n",a);
     printf("The swap value of b: %d",b);
 }
+
+//Swapping two numbers using pointer method with void
+
+#include<stdio.h>
+void swap(int *x,int *y){
+    *x=*x+*y;
+    *y=*x-*y;
+    *x=*x-*y;
+    return;
+}
+int main(){
+    int a;
+    printf("Enter number a: ");
+    scanf("%d",&a);
+    int b;
+    printf("Enter number b: ");
+    scanf("%d",&b);
+    swap(&a,&b);
+    printf("The swap value of a: %d\n",a);
+    printf("The swap value of b: %d",b);
+}
